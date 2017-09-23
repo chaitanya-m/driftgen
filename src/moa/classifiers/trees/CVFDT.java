99c50aa01a6ee155ffa5a4cfc06c51d8b483a48a
Chaitanya Manapragada <cman39@student.monash.edu.au>
2017-09-23 16:07:53 +1000

    CVFDT doesn't need nodeTime- it has nodeTrainingTime which already
    handles this...
    
    Also, updating in accordance with the name change

--- src/moa/classifiers/trees/CVFDT.java
+++ src/moa/classifiers/trees/CVFDT.java
@@ -24,12 +24,12 @@ import moa.classifiers.core.attributeclassobservers.AttributeClassObserver;
 import moa.classifiers.core.conditionaltests.InstanceConditionalTest;
 import moa.classifiers.core.splitcriteria.SplitCriterion;
 import moa.classifiers.trees.VFDT.Node;
-import moa.classifiers.trees.VFDTSlidingWindow.AdaNode;
-import moa.classifiers.trees.VFDTSlidingWindow.AdaSplitNode;
+import moa.classifiers.trees.VFDTGlobalWindow.AdaNode;
+import moa.classifiers.trees.VFDTGlobalWindow.AdaSplitNode;
 import moa.core.AutoExpandVector;
 import moa.core.Utils;
 
-public class CVFDT extends VFDTSlidingWindow {
+public class CVFDT extends VFDTGlobalWindow {
 
 	// lets begin by adding counters and split tests to every splitnode
 	// then we can add a list of alternates to grow the results of the split tests
@@ -54,8 +54,6 @@ public class CVFDT extends VFDTSlidingWindow {
 		public boolean isTopAlternate();
 
 		public void setTopAlternate(boolean isTopAlt);
-
-		public int getNodeTime();
 
 	}
 
@@ -143,7 +141,7 @@ public class CVFDT extends VFDTSlidingWindow {
 		}
 
 		@Override
-        public void forgetInstance(Instance inst, VFDTSlidingWindow ht, AdaSplitNode parent, int parentBranch, long maxNodeID) {
+        public void forgetInstance(Instance inst, VFDTGlobalWindow ht, AdaSplitNode parent, int parentBranch, long maxNodeID) {
 
             // DRY... for now this code is repeated...
             // Updates statistics in split nodes also
@@ -189,10 +187,8 @@ public class CVFDT extends VFDTSlidingWindow {
 
 
 		@Override
-		public void learnFromInstance(Instance inst, VFDTSlidingWindow ht, SplitNode parent, int parentBranch,
+		public void learnFromInstance(Instance inst, VFDTGlobalWindow ht, SplitNode parent, int parentBranch,
 				AutoExpandVector<Long> reachedLeafIDs){
-
-			nodeTime++;
 
 			// only mainline split nodes are capable of launching a test phase
 			// alternate nodes shouldn't get to the point of actually learning
@@ -614,7 +610,7 @@ public class CVFDT extends VFDTSlidingWindow {
 			}
 
 			@Override
-			public void learnFromInstance(Instance inst, VFDTSlidingWindow ht, SplitNode parent, int parentBranch,
+			public void learnFromInstance(Instance inst, VFDTGlobalWindow ht, SplitNode parent, int parentBranch,
 					AutoExpandVector<Long> reachedLeafIDs) {
 
 				super.learnFromInstance(inst, ht, parent, parentBranch, reachedLeafIDs);

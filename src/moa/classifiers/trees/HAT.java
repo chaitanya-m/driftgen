2f64d3826b29fbc981ed6f03b32d9c1e6e8143e7
Chaitanya <chait.m@zoho.com>
2019-06-05 06:19:10 +0000

    The previous changes made it really neat to derive HAT etc from EFDT or VFDT as the primary split criteria have been abstracted away

--- src/moa/classifiers/trees/HAT.java
+++ src/moa/classifiers/trees/HAT.java
@@ -718,66 +718,16 @@ public class HAT extends VFDT {
     @Override
 	protected void attemptToSplit(ActiveLearningNode node, SplitNode parent,
             int parentIndex) {
+    	
+    	
         if (!node.observedClassDistributionIsPure()) {
+        	
+			node.addToSplitAttempts(1); // even if we don't actually attempt to split, we've computed infogains
             SplitCriterion splitCriterion = (SplitCriterion) getPreparedClassOption(this.splitCriterionOption);
             AttributeSplitSuggestion[] bestSplitSuggestions = node.getBestSplitSuggestions(splitCriterion, this);
-            Arrays.sort(bestSplitSuggestions);
-            boolean shouldSplit = false;
-            if (bestSplitSuggestions.length < 2) {
-                shouldSplit = bestSplitSuggestions.length > 0;
-            } else {
-                double hoeffdingBound = computeHoeffdingBound(splitCriterion.getRangeOfMerit(node.getObservedClassDistribution()),
-                        this.splitConfidenceOption.getValue(), node.getWeightSeen());
-                AttributeSplitSuggestion bestSuggestion = bestSplitSuggestions[bestSplitSuggestions.length - 1];
-                AttributeSplitSuggestion secondBestSuggestion = bestSplitSuggestions[bestSplitSuggestions.length - 2];
-                if ((bestSuggestion.merit - secondBestSuggestion.merit > hoeffdingBound)
-                        || (hoeffdingBound < this.tieThresholdOption.getValue())) {
-                    shouldSplit = true;
-                }
+            
 
-                if(shouldSplit){
-                	for(Integer i : node.usedNominalAttributes){
-                		if(bestSuggestion.splitTest.getAttsTestDependsOn()[0] == i){
-                			shouldSplit = false;
-                			break;
-                		}
-                	}
-                }
-
-                // }
-                if ((this.removePoorAttsOption != null)
-                        && this.removePoorAttsOption.isSet()) {
-                    Set<Integer> poorAtts = new HashSet<Integer>();
-                    // scan 1 - add any poor to set
-                    for (int i = 0; i < bestSplitSuggestions.length; i++) {
-                        if (bestSplitSuggestions[i].splitTest != null) {
-                            int[] splitAtts = bestSplitSuggestions[i].splitTest.getAttsTestDependsOn();
-                            if (splitAtts.length == 1) {
-                                if (bestSuggestion.merit
-                                        - bestSplitSuggestions[i].merit > hoeffdingBound) {
-                                    poorAtts.add(new Integer(splitAtts[0]));
-                                }
-                            }
-                        }
-                    }
-                    // scan 2 - remove good ones from set
-                    for (int i = 0; i < bestSplitSuggestions.length; i++) {
-                        if (bestSplitSuggestions[i].splitTest != null) {
-                            int[] splitAtts = bestSplitSuggestions[i].splitTest.getAttsTestDependsOn();
-                            if (splitAtts.length == 1) {
-                                if (bestSuggestion.merit
-                                        - bestSplitSuggestions[i].merit < hoeffdingBound) {
-                                    poorAtts.remove(new Integer(splitAtts[0]));
-                                }
-                            }
-                        }
-                    }
-                    for (int poorAtt : poorAtts) {
-                        node.disableAttribute(poorAtt);
-                    }
-                }
-            }
-            if (shouldSplit) {
+            if (decideToSplitAndPrune(node, parent, parentIndex, bestSplitSuggestions, splitCriterion)) {
             	splitCount++;
                 AttributeSplitSuggestion splitDecision = bestSplitSuggestions[bestSplitSuggestions.length - 1];
                 if (splitDecision.splitTest == null) {

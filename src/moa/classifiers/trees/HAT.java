4a506a68350e8118b6c2a040a06828e57bacce6d
Chaitanya <chait.m@zoho.com>
2019-08-16 00:50:07 +0000

    For whatever reason I can't write directly to the old file name (previously cdgen3, now mytrees). I have to create a new file and copy it back in. Also updated HAT and HATEFDT to optionally allow alternate voting.

--- src/moa/classifiers/trees/HAT.java
+++ src/moa/classifiers/trees/HAT.java
@@ -39,6 +39,8 @@ import moa.classifiers.trees.VFDT.SplitNode;
 import moa.core.DoubleVector;
 import moa.core.MiscUtils;
 import moa.core.Utils;
+
+import com.github.javacliparser.FlagOption;
 import com.yahoo.labs.samoa.instances.Instance;
 
 /**
@@ -62,6 +64,8 @@ import com.yahoo.labs.samoa.instances.Instance;
  */
 public class HAT extends VFDT {
 
+    public FlagOption alternateVoterOption = new FlagOption("alternatesVote", 'A',
+            "Allow alternates to vote");
     private static final long serialVersionUID = 1L;
 
     private static long numInstances = 0;
@@ -990,7 +994,7 @@ public class HAT extends VFDT {
     					}
     					double[] dist = leafNode.getClassVotes(inst, this);
 
-    					if(!((NewNode)leafNode).isAlternate()){
+    					if(!((NewNode)leafNode).isAlternate() || alternateVoterOption.isSet()){
 
     						// count only votes from non-alternates... alternates shouldn't be voting
     						result.addValues(dist);
